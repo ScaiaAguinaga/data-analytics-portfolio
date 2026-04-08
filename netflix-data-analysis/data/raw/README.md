fb55bf04f506210aca6b6c31c4637ed6a1fa39e6
Isaac <103446311+ScaiaAguinaga@users.noreply.github.com>
2026-04-08 01:02:31 -0700

    Made updates to explore_data script and raw data README

--- netflix-data-analysis/data/raw/README.md
+++ netflix-data-analysis/data/raw/README.md
@@ -10,3 +10,28 @@ To use this project:
 - Place it in this folder before running the scripts
 
 No modifications should be made to files in this directory.
+
+# WIP LIST
+
+## Issues Found
+	- Large missing values in 'director' (~30%), 'cast' (~9%), and 'country' (~9%)
+	- Negligible missing values in 'date_added' (10ct), 'rating' (4ct), duration (3ct)
+	- Inconsistent data in 'rating' column (some entries contain movie duration instead)
+	- Multiple values stored in single fields (e.g., multiple countries per entry)
+
+## Possible Analyses
+	- Content breakdown by genre (identifies viewer interest by proxy of Netflix development focus)
+	- content breakdown by rating (identifies target audience)
+	- Content growth over time
+	- What countries produces the most content?
+	- Basic movie/show stats (longest, shortest, average, most common genre)
+	- How often does Netflix wait to add a movie or show on average (average difference between release year and date added)
+	- Trend discovery (Has Netflix been adding more/less of a specific genre as time goes on)
+
+## Cleaning Needed
+	- Fill missing directors and casts with "Unknown".
+	- Handle missing "country" values.
+	- Ignore minor missing values as they do not have a realized impact on analysis outcomes.
+	- Handle duration values in rating column
+	- convert date added to be in a standardized and usable format for arithmetic.
+	- Add 'grace_period' column to determine how long a show/movie was released before being added to the Netflix catalog.
--- netflix-data-analysis/scripts/explore_data.py
+++ netflix-data-analysis/scripts/explore_data.py
@@ -3,14 +3,47 @@ import pandas as pd
 file_path = "data/raw/netflix_titles.csv"
 df = pd.read_csv(file_path)
 
-print("\nDATAFRAME INFO:")
-print(df.info())
+# Displays shape of dataframe
+print("\n=== OVERVIEW ===")
+print(f"Shape: {df.shape}")
 
-print("\nMISSING DATA:")
-print(df.isna().sum())
+# Displays column tags
+print("\nCOLUMNS: ")
+print(df.columns.tolist())
 
-invalid_ratings = df[df["rating"].str.contains("min", na=False)]
+# Displays first few rows for visual understanding
+print("\nFIRST 5 ROWS: ")
+print(df.head())
 
-print(invalid_ratings["rating"])
+# Displays column information
+print("\nDATAFRAME INFO: ")
+df.info()
 
-print(invalid_ratings["rating"].count())
+# Displays subtotal of missing entries per column
+print("\nMISSING VALUES: ")
+missing_values = df.isna().sum().sort_values(ascending=False)
+print(missing_values)
+
+# Displays missing entries in percent format
+print("\nMISSING VALUES: ")
+missing_percent = round((df.isna().sum() / len(df) * 100), 2)
+print(f"{missing_percent.sort_values(ascending=False).astype(str) + '%'}")
+
+# Displays count of duplicate rows
+print("\nDUPLICATES: ")
+print(df.duplicated().sum())
+
+# Displays descriptive statistics
+# Not very useful in this case since most data is string based
+# print("\nDESCRIPTION: ")
+# print(df.describe(include="all"))
+
+# Displays all unique valus with associated counts
+print("\nUNIQUE VALUE CHECKS:")
+columns_to_check = ["type", "country", "rating"]
+
+for col in columns_to_check:
+  print(f"\nColumn: {col}")
+  # For investigating entire list of unique entries
+  # print(df[col].unique().tolist())
+  print(df[col].value_counts(dropna=False).head(10))
